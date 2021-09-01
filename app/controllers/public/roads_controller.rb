6ec2cd5c41d80286a934202a52f29dbd3d1ca5f2
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-01 09:52:05 +0000

    add view new

--- app/controllers/public/roads_controller.rb
+++ app/controllers/public/roads_controller.rb
@@ -1,14 +1,24 @@
 class Public::RoadsController < ApplicationController
   def index
+    @roads = Road.all
   end
 
   def show
+    @road = Road.find(params[:id])
   end
 
   def new
+    @road = Road.new
   end
 
   def create
+    @road = Road.new(road_params)
+    if @road.save
+      flash[:notice] = '新しいサイクリングロードの登録が完了しました'
+      redirect_to road_path(@road)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +28,10 @@ class Public::RoadsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def road_params
+    params.require(:road).permit(:name, :introduction, :start_latitude, :start_longitude, :goal_latitude, :goal_longitude, :distance, :elevation_gain, :difficulty, :is_editable )
   end
 end
