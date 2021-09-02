7fa42ec7a0d6dfbb239edf701c7b5ed8af097215
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-02 08:42:20 +0000

    first commit

---
+++ app/controllers/public/check_points_controller.rb
@@ -0,0 +1,48 @@
+class Public::CheckPointsController < ApplicationController
+
+  def show
+    @road = Road.find(params[:road_id])
+    @check_point = CheckPoint.find(params[:id])
+  end
+
+  def new
+    @road = Road.find(params[:road_id])
+    @check_point = CheckPoint.new
+  end
+
+  def create
+    @road = Road.find(params[:road_id])
+    @check_point = CheckPoint.new(check_point_params)
+    @check_point.user_id = current_user.id
+    @check_point.road_id = @road.id
+    if @check_point.save
+      flash[:notice] = "チェックポイントが登録されました"
+      redirect_to road_path(@check_point.road)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @check_point = CheckPoint.find(params[:id])
+  end
+
+  def update
+    @check_point = CheckPoint.find(params[:id])
+    if @check_point.update(check_point_params)
+      flash[:notice] = "チェックポイントの更新ができました"
+      redirect_to road_checkpoint_path(@check_point.road, @check_point)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    CheckPoint.find(params[:id]).destroy
+  end
+
+  private
+  def check_point_params
+    params.require(:check_point).permit(:name, :introduction, :latitude, :longitude)
+  end
+end
---
+++ test/controllers/public/check_points_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Public::CheckPointsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
