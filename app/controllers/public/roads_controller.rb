10263095805705144213af052ccaf5144aab706f
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-02 06:59:18 +0000

    add CRUD

--- app/controllers/public/roads_controller.rb
+++ app/controllers/public/roads_controller.rb
@@ -13,6 +13,7 @@ class Public::RoadsController < ApplicationController
 
   def create
     @road = Road.new(road_params)
+    @road.user_id = current_user.id
     if @road.save
       flash[:notice] = '新しいサイクリングロードの登録が完了しました'
       redirect_to road_path(@road)
@@ -22,16 +23,25 @@ class Public::RoadsController < ApplicationController
   end
 
   def edit
+    @road = Road.find(params[:id])
   end
 
   def update
+    @road = Road.find(params[:id])
+    if @road.update(road_params)
+      flash[:notice] = 'サイクリングロードの更新が完了しました'
+      redirect_to road_path(@road)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Road.find(params[:id]).destroy
   end
 
   private
   def road_params
-    params.require(:road).permit(:name, :introduction, :start_latitude, :start_longitude, :goal_latitude, :goal_longitude, :distance, :elevation_gain, :difficulty, :is_editable )
+    params.require(:road).permit(:name, :introduction, :start_latitude, :start_longitude, :goal_latitude, :goal_longitude, :distance, :elevation_gain, :difficulty, :is_editable, :image )
   end
 end
