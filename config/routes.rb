4331635b5bfd5e65811dab96d850b3243db28321
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-06 03:38:20 +0000

    add user edit and unsubscribe

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
     sessions: 'admin/sessions',
   }
-  
+
   devise_for :users, controllers: {
     sessions: 'public/sessions',
     registrations: 'public/registrations',
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root 'roads#index'
     get 'users/mypage' =>            'users#show', as: 'mypage'
-    get 'users/information/edit' =>  'users#edit', as:'edit_informateion'
+    get 'users/information/edit' =>  'users#edit', as:'edit_information'
     patch 'users/information' =>     'users#update', as:'update_information'
     put 'users/informateion' =>      'users#update'
     get 'users/unsubscribe' =>       'users#unsubscribe', as:'confirm_unsubscribe'
