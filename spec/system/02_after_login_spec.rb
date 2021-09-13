1464dde3f08caeb9e85fa17afe98c1180bd1dc3d
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-13 10:09:07 +0000

    add RSpeq

--- spec/system/02_after_login_spec.rb
+++ spec/system/02_after_login_spec.rb
@@ -4,15 +4,35 @@ RSpec.feature 'ログイン後の操作' do
   background do
     Road.create!(name: '淀川サイクリングロード', introduction: "淀川沿いに、大阪の中心から京都まで行くルート。", start_latitude: 34.72182943880068, start_longitude: 135.5171911744655, goal_latitude: 34.89057364482206, goal_longitude: 135.70140959744964, distance: 27, elevation_gain: 65, difficulty: "normal", is_editable: nil, area:4)
     User.create!(first_name: '太郎', last_name: '山田', nick_name: '山ちゃん', email: 'foo@example.com', password: 'taroyamada', introduction: '山田です。よろしくお願いします。' )
-  end
-  scenario 'ログイン画面からログインする' do
+
+    #ログイン操作
     visit new_user_session_path
     fill_in 'user_email', with: 'foo@example.com'
     fill_in 'user_password', with: 'taroyamada'
     click_on 'Log in'
+  end
+  scenario 'ログインする' do
+    # visit new_user_session_path
+    # fill_in 'user_email', with: 'foo@example.com'
+    # fill_in 'user_password', with: 'taroyamada'
+    # click_on 'Log in'
     expect(page).to have_content 'ログインしました。'
   end
 
   scenario 'ロードの地図リンクをクリックし、ロード詳細画面に遷移する' do
+    p User.find(1)
+    click_on '淀川サイクリングロード'
+    expect(current_path).to eq road_path(1)
   end
+
+  scenario '' do
+  end
+
+  scenario 'ログアウトする' do
+  end
+
+  scenario '新規ユーザー登録する' do
+  end
+
+  scenario ''
 end
