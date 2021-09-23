82861d86fb9d6a32f07164ba3e844a90c2a1e727
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-23 06:59:37 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#pushされていないかテスト
-
+Admin.create!(
+  email:    ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASS']
+  )
