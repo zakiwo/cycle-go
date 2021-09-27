4f243f9b2a844935acb7b0af1416b5bf79c7be73
Zakiwo <hiroki.ozaki39@gmail.com>
2021-09-27 10:02:01 +0000

    waypoint admin

---
+++ app/controllers/admin/waypoints_controller.rb
@@ -0,0 +1,61 @@
+class Admin::WaypointsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def new
+    @road = Road.find(params[:road_id])
+    @waypoint = Waypoint.new
+  end
+
+  def create
+    @road = Road.find(params[:road_id])
+    @waypoint = Waypoint.new(waypoint_params)
+    @waypoint.road_id = params[:road_id]
+    @waypoint.user_id = current_user.id
+    #経由順番の最後を取得し、プラス1の値を付与
+    if last_waypoint = @road.waypoints.order(:point_no).last
+      @waypoint.point_no = last_waypoint.point_no + 1
+    else
+      @waypoint.point_no = 1
+    end
+    if @waypoint.save
+      flash[:notice] = '新しい経由地点の登録が完了しました'
+      redirect_to admin_road_path(@road)
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @road = Road.find(params[:road_id])
+    @waypoint = Waypoint.find(params[:id])
+  end
+
+  def update
+    road = Road.find(params[:road_id])
+    @waypoint = Waypoint.find(params[:id])
+    if @waypoint.update(waypoint_params)
+      flash[:notice] = '経由地点の更新が完了しました'
+      redirect_to admin_road_path(road)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    road = Road.find(params[:road_id])
+    Waypoint.find(params[:id]).destroy
+    flash[:notice] = "経由地点を削除しました"
+    redirect_to admin_road_path(road)
+  end
+
+  private
+
+  def waypoint_params
+    params.require(:waypoint).permit(
+      :name,
+      :latitude,
+      :longitude,
+      :point_no,
+    )
+  end
+end
